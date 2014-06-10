f51f9604e9ce995f2baa3f859d098db78732e00f
Patrick Mauro <me@patrickmauro.com>
2014-06-10 13:51:05 -0400

    Added puma-mapping.csv, a table that lists each PUMA and all meaningfully adjacent PUMAs.  I say 'meaningfully adjacent' because if Long Island Sound separates two PUMAs - one in the Bronx, one in Queens - those PUMAs are not contiguous for the purposes of food distribution.
    Modified agency_targeting.py to allocate food for regions for which there
    are no agencies.  Also made minor cleanups.

--- agency_targeting.py
+++ agency_targeting.py
@@ -2,33 +2,32 @@
 import csv
 import re
 
-from collections import defaultdict
-
-#todo don't require Excel cleanup
-
-INPUT_FILE = "/Users/patrickmauro/Documents/20140517 - CH Targeting Model.csv"
+AGENCY_INPUT_FILE = "/Users/patrickmauro/Documents/Agency Targets.csv"
+REGION_INPUT_FILE = "/Users/patrickmauro/Documents/Regional Targets.csv"
 PUMA_MAP_FILE = "/Users/patrickmauro/code/ch/puma-mapping.csv"
-
-# We might not want to fill an agency to its capacity if we're worried about other emergency food programs filling up
-# an agency's capacity.  CAPACITY_FACTOR is the percentage of real capacity that we use as our capacity threshold for
-# each agency.  That is, a CAPACITY_FACTOR of 0.9 means we'll fill each agency up to 90% capacity.
-CAPACITY_FACTOR = 1.0
 
 VERBOSE = True
 
 # -------------------------------------------------------------
 
+
+def str_to_int(input_str):
+    val = re.sub("[, ]", "", input_str.rstrip())
+    val = int(val) if val.isdigit() else None
+    return val
+
+
 class Region():
-    def __init__(self, region_id, ef_demand, neighbors):
-        self.id = region_id
-        self.ef_demand = ef_demand
-        self.neighboring_regions = neighbors
+    def __init__(self, _id, _ef_demand, _neighbors):
+        self.id = _id
+        self.ef_demand = _ef_demand
+        self.neighboring_regions = _neighbors
 
         self.agencies = []
         self.overage = 0
 
-    def add_agency(self, agency):
-        self.agencies.append(agency)
+    def add_agency(self, _agency):
+        self.agencies.append(_agency)
 
     # returns unallocated portion of distrib_amt
     # allocates by filling agencies to capacity, from agency with most bandwidth to agency with least bandwidth
@@ -37,111 +36,37 @@ class Region():
         available_agencies = filter(lambda x: x.get_overage() < 0, self.agencies)
 
         # Allocate food from agency with most bandwidth to agency with least bandwidth
-        for agency in sorted(available_agencies, key=lambda x: x.bandwidth, reverse=True):
-            amt = min(distrib_amt, agency.adj_capacity - agency.cur_tgt)
-            agency.cur_tgt += amt
+        for tgt_agency in sorted(available_agencies, key=lambda x: x.bandwidth, reverse=True):
+            amt = min(distrib_amt, tgt_agency.get_capacity())
+            tgt_agency.cur_tgt += amt
             distrib_amt -= amt
 
+            # should never be < 0, but let's be a bit silly here to be super-safe
             if distrib_amt <= 0:
                 break
 
         return distrib_amt
 
+
 class Agency():
-    def __init__(self, ch_id, tgt_pounds, capacity, bandwidth):
-        self.ch_id = ch_id
-        self.init_tgt = tgt_pounds
-        self.cur_tgt = tgt_pounds
-        self.true_capacity = capacity
-        self.adj_capacity = capacity * CAPACITY_FACTOR
-        self.bandwidth = bandwidth
+    def __init__(self, _ch_id, _tgt_pounds, _capacity, _bandwidth):
+        self.ch_id = _ch_id
+        self.init_tgt = _tgt_pounds
+        self.cur_tgt = _tgt_pounds
+        self.capacity = _capacity
+        self.bandwidth = _bandwidth
 
     def get_overage(self):
-        return self.cur_tgt - self.adj_capacity
+        return self.cur_tgt - self.capacity
 
     def get_capacity(self):
-        return self.adj_capacity
-
-# -------------------------------------------------------------
+        return -1.0 * self.get_overage()
 
 
-# Step 1 - Read input files
-region_map = dict()
-#todo put error checking from check_puma_mapping.py in here
-with open(PUMA_MAP_FILE, 'rbU') as csvfile:
-    reader = csv.reader(csvfile)
-
-    # skip the header rows
-    next(reader, None)
-
-    for row in reader:
-        region_id = int(row[0])
-        neighbors = map(lambda x: int(x), row[1].split(","))
-
-        if region_id in region_map:
-            print("Region id %d appears twice in region mapping file" % region_id)
-        else:
-            region_map[region_id] = set(neighbors)
-
-regions = dict() # region_id -> Region
-with open(INPUT_FILE, 'rbU') as csvfile:
-    reader = csv.reader(csvfile)
-
-    # skip the header rows
-    next(reader, None)
-
-    for row in reader:
-        # get rid of the unicode nonsense (thanks MSFT!)
-        row = map(lambda x: str(x.decode("ascii", "ignore")), row)
-
-        if row[0] == "":
-            continue
-
-        agency_name = row[0]
-        ch_id = row[1]
-        # region_name = row[2]
-        region_id = int(row[2])
-        region_demand = int(row[3])
-        meals_served = int(row[4])
-        agency_region_count = int(row[5])
-        pctg = row[6] # todo fix output format from excel
-
-        tgt_pounds = re.sub("[, ]", "", row[7].rstrip())
-        tgt_pounds = int(tgt_pounds) if tgt_pounds.isdigit() else 0
-
-        capacity = re.sub("[, ]", "", row[8].rstrip())
-        capacity = int(capacity) if capacity.isdigit() else 0
-
-        if region_id not in regions:
-            if region_id not in region_map:
-                print("Could not find region->neighbor mapping for region %d" % region_id)
-                continue
-            regions[region_id] = Region(region_id, region_demand, region_map[region_id])
-        else:
-            if regions[region_id].ef_demand != region_demand:
-                print("Differing region demands ( %d v. %d ) for region %d" %
-                    (regions[region_id].ef_demand, region_demand, region_id))
-
-        regions[region_id].add_agency(Agency(ch_id, tgt_pounds, capacity, meals_served))
-
-
-# Step 2 - Redistribute within a region, from agency with most bandwidth to least bandwidth
-for region in regions.values():
-    #todo Move this to Region method?
-    overage = 0
-    for agency in region.agencies:
-        if agency.get_overage() > 0:
-            overage += agency.get_overage()
-            agency.cur_tgt = agency.get_capacity()
-
-    if overage > 0:
-        region.overage = region.distrib_overage(overage)
-
-# Step 3 - Redistribute among neighboring regions, from region with most need to region with least need
 def distrib_btwn_regions(restrict_to_neighbors):
     # a) only distribute to regions w/o overage b/c such regions are already full
     # b) sort from most need to least need
-    # c) make this a list of region ids
+    # c) make this a list of region ids, not Region objects
     available_regions = filter(lambda x: x.overage == 0, regions.values())
     available_regions = sorted(available_regions, key=lambda x: x.ef_demand, reverse=True)
     available_regions = map(lambda x: x.id, available_regions)
@@ -149,6 +74,7 @@ def distrib_btwn_regions(restrict_to_neighbors):
     # now start distributing the overage
     regions_w_overage = filter(lambda x: x.overage > 0, regions.values())
     # start sorting from the region with the most overage to make this algorithm more stable
+    #todo Is this claim of greater stability true?
     for source_region in sorted(regions_w_overage, key=lambda x: x.overage, reverse=True):
         if restrict_to_neighbors:
             # Get neighboring regions in the order in which we'll check them
@@ -170,6 +96,109 @@ def distrib_btwn_regions(restrict_to_neighbors):
             if source_region.overage == 0:
                 break
 
+# -------------------------------------------------------------
+
+# Step 1 - Read input files
+neighbor_map = dict()
+#todo put error checking from check_puma_mapping.py in here
+with open(PUMA_MAP_FILE, 'rbU') as csvfile:
+    reader = csv.reader(csvfile)
+
+    # skip the header rows
+    next(reader, None)
+
+    for row in reader:
+        region_id = int(row[0])
+        neighbors = map(lambda x: int(x), row[1].split(","))
+
+        if region_id in neighbor_map:
+            print("Region id %d appears twice in region mapping file" % region_id)
+        else:
+            neighbor_map[region_id] = set(neighbors)
+
+region_to_demand = dict()
+region_to_target = dict()
+with open(REGION_INPUT_FILE, 'rbU') as csvfile:
+    reader = csv.reader(csvfile)
+
+    # skip the header rows
+    next(reader, None)
+
+    for row in reader:
+        if not row[0].isdigit():
+            continue
+
+        region_id = int(row[0])
+        name = row[1]
+        ppl_in_need = str_to_int(row[2])
+        ef_demand_total = str_to_int(row[3])
+        ef_demand_satisfied = str_to_int(row[4])
+        ef_demand_residual = str_to_int(row[5])
+        region_target = str_to_int(row[6])
+
+        region_to_demand[region_id] = ef_demand_residual
+        region_to_target[region_id] = region_target
+
+regions = dict()  # region_id -> Region
+with open(AGENCY_INPUT_FILE, 'rbU') as csvfile:
+    reader = csv.reader(csvfile)
+
+    # skip the header rows
+    next(reader, None)
+
+    for row in reader:
+        # get rid of the unicode nonsense (thanks MSFT!)
+        row = map(lambda x: str(x.decode("ascii", "ignore")), row)
+
+        if row[0] == "":
+            continue
+
+        agency_name = row[0]
+        ch_id = row[1]
+        efro = int(row[2])
+        region_id = int(row[3])
+        meals_served = int(row[4])
+        agency_region_count = int(row[5])
+        pctg = row[6]  # todo fix output format from excel
+        tgt_pounds = str_to_int(row[7])
+        capacity = str_to_int(row[8])
+
+        if region_id not in regions:
+            if region_id not in neighbor_map:
+                print("Could not find region->neighbor mapping for region %d" % region_id)
+                continue
+
+            if region_id not in region_to_demand:
+                print("Could not find region->demadn mapping for region %d" % region_id)
+                continue
+
+            regions[region_id] = Region(region_id, region_to_demand[region_id], neighbor_map[region_id])
+
+        regions[region_id].add_agency(Agency(ch_id, tgt_pounds, capacity, meals_served))
+
+# Some regions don't have agencies.  Set the overage for those regions appropriately.
+regions_wo_agencies = set(region_to_target.keys()) - set(regions.keys())
+for region_id in regions_wo_agencies:
+    if region_id not in neighbor_map:
+        print("Could not find region->neighbor mapping for region %d" % region_id)
+        continue
+
+    regions[region_id] = Region(region_id, region_to_demand[region_id], neighbor_map[region_id])
+    regions[region_id].overage = region_to_target[region_id]
+
+# Step 2 - Redistribute within a region, from agency with most bandwidth to least bandwidth
+for region in regions.values():
+    #todo Move this to Region method?
+    overage = 0
+    for agency in region.agencies:
+        if agency.get_overage() > 0:
+            overage += agency.get_overage()
+            agency.cur_tgt = agency.capacity
+
+    if overage > 0:
+        region.overage = region.distrib_overage(overage)
+
+# Step 3 - Redistribute among neighboring regions, from region with most need to region with least need
 distrib_btwn_regions(True)
 
 # Step 4 - Deal with remainder: Redistribute among regions, from region with most need to region with least need
